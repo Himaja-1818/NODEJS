cd974ee10df206caf05cc506701db5fa9ae3300a
himaja <himajagdr.1818@gmail.com>
2019-10-25 10:50:15 +0530

    'commit'

---
+++ console.js
@@ -0,0 +1,10 @@
+const chalk=require('chalk');
+console.time('add');
+let a=90;
+let b=20;
+add=a+b;
+console.log(add);
+console.log(chalk.blue(add));
+// console.warn(chalk.yellow(add));
+// console.error(chalk.red(add));
+console.timeEnd('add');
