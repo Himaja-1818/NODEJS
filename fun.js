abb100f2c752d7ed6d55fc5d4eb0154a91f4646b
himaja <himajagdr.1818@gmail.com>
2019-10-28 09:50:21 +0530

    functions

---
+++ fun.js
@@ -0,0 +1,9 @@
+function add(a,b,c)
+{
+    return(a+b+c);
+}
+let result=add(1,2,3);
+console.log(result);
+let result1=add(8,4,6);
+console.log(result1);
+
---
+++ fun1.js
@@ -0,0 +1,12 @@
+let fun1=(a,b,c)=>            //arrow functions
+{
+    return(a+b+c);
+}
+let result=fun1(1,2,3)
+console.log(result);
+//if only one argument  all brackets and return are removed
+let fun=a=>
+(a+3);
+
+result=fun(2);
+console.log(result);
